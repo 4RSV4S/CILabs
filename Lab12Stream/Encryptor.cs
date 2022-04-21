fb22f9195f0aa3713db12ed6c7c82fe4ba83452f
Arseni Vasenda <vasenda2000@yandex.ru>
2022-04-22 00:50:27 +0300

    Create Lab12Stream

---
+++ Lab12Stream/Encryptor.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab12Stream
+{
+    internal class Encryptor
+    {   
+        private readonly string Table = " абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ0123456789";
+        byte[] S = new byte[256];
+
+        int x = 0;
+        int y = 0;
+
+        // Key-Scheduling Algorithm 
+        // Алгоритм ключевого расписания 
+        public void init(byte[] key)
+        {
+            int keyLength = key.Length;
+
+            for (int i = 0; i < 256; i++)
+            {
+                S[i] = (byte)i;
+            }
+
+            int j = 0;
+            for (int i = 0; i < 256; i++)
+            {
+                j = (j + S[i] + key[i % keyLength]) % 256;
+                S.Swap(i, j);
+            }
+        }
+
+        public byte[] Encrypt(byte[] dataB, int size)
+        {
+            byte[] data = dataB.Take(size).ToArray();
+
+            byte[] cipher = new byte[data.Length];
+
+            for (int m = 0; m < data.Length; m++)
+            {
+                cipher[m] = (byte)(data[m] ^ keyItem());
+            }
+
+            return cipher;
+        }
+        public byte[] Decrypt(byte[] dataB, int size)
+        {
+            return Encrypt(dataB, size);
+        }
+
+        // Pseudo-Random Generation Algorithm 
+        // Генератор псевдослучайной последовательности 
+        private byte keyItem()
+        {
+            x = (x + 1) % 256;
+            y = (y + S[x]) % 256;
+
+            S.Swap(x, y);
+
+            return S[(S[x] + S[y]) % 256];
+        }
+    }
+
+    static class SwapExt
+    {
+        public static void Swap<T>(this T[] array, int index1, int index2)
+        {
+            T temp = array[index1];
+            array[index1] = array[index2];
+            array[index2] = temp;
+        }
+    }
+}
---
+++ Lab12Stream/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Text;
+
+namespace Lab12Stream
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[] key = new byte[100];
+            string strKey;
+            string testString = "Plaintext";
+            byte[] testBytes = new byte[100];
+            byte[] result = new byte[100];
+            byte[] decryptedBytes = new byte[100];
+            Input();
+            void Input()
+            {
+                Console.WriteLine("Выберите действие:\n{1} Зашифровать текст, используя ключ\n{2} Дешифровать текст, используя ключ\n{3} Выход");
+                string action = Console.ReadLine();
+                while (action != "1" && action != "2" && action != "3")
+                {
+                    Console.WriteLine("Выберите действие (1-3).");
+                    action = Console.ReadLine();
+                }
+                switch (action)
+                {
+                    case "1":
+                        Encryptor encoder = new Encryptor();
+                        Console.WriteLine("Введите текст:");
+                        testString = Console.ReadLine();
+                        while (testString.Length > 100)
+                        {
+                            Console.WriteLine("Текст не может быть длиннее 100 символов. Повторите ввод.");
+                            testString = Console.ReadLine();
+                        }
+                        Console.WriteLine("Введите ключ:");
+                        strKey= Console.ReadLine();
+                        while (strKey.Length > 20 || strKey.Length < 4)
+                        {
+                            Console.WriteLine("Ключ не может быть короче 4 символов либо длиннее 20. Повторите ввод.");
+                            strKey = Console.ReadLine();
+                        }
+                        key = ASCIIEncoding.ASCII.GetBytes(strKey);
+                        encoder.init(key);
+                        testBytes = Encoding.ASCII.GetBytes(testString);
+                        result = encoder.Encrypt(testBytes, testBytes.Length);
+                        Console.WriteLine($"Зашифрованный текст:\n{ASCIIEncoding.ASCII.GetString(result)}");
+                        Input();
+                        break;
+                    case "2":
+                        Encryptor decoder = new Encryptor();
+                        Console.WriteLine("Введите ключ:");
+                        strKey = Console.ReadLine();
+                        while (strKey.Length > 20 || strKey.Length < 4)
+                        {
+                            Console.WriteLine("Ключ не может быть короче 4 символов либо длиннее 20. Повторите ввод.");
+                            strKey = Console.ReadLine();
+                        }
+                        key = ASCIIEncoding.ASCII.GetBytes(strKey);
+                        decoder.init(key);
+                        byte[] decryptedBytes = decoder.Decrypt(result, result.Length);
+                        string decryptedString = ASCIIEncoding.ASCII.GetString(decryptedBytes);
+                        Console.WriteLine($"{decryptedString}");
+                        Input();
+                        break;
+                    case "3":
+                        break;
+                }
+            }
+        }
+    }
+}
